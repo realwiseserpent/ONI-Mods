d491a136656178751abb5923c414a1b191db485e
RealWiseSerpent <74019811+realwiseserpent@users.noreply.github.com>
2024-08-22 21:43:57 +0400

    Fervine - Translation support

--- src/Fervine/FervineConfig.cs
+++ src/Fervine/FervineConfig.cs
@@ -8,14 +8,7 @@ namespace Fervine
 	public class FervineConfig : IEntityConfig
 	{
 		public const string Id = "Fervine";
-		public const string Name = "Fervine";
-		public static string Description = $"A temperature reactive, subterranean {UI.FormatAsLink("Plant", "PLANTS")}.";
-		public static string DomesticatedDescription = $"Fervine uses tiny amounts of heat energy from the atmosphere to keep its light up. " +
-		                                               $"It won't use any if the temperature falls under {GameUtil.GetFormattedTemperature(293.15f)}.";
-
 		public const string SeedId = "FervineBulb";
-		public const string SeedName = "Fervine Bulb";
-		public static string SeedDesc = $"The {UI.FormatAsLink("Seed", "PLANTS")} of a {UI.FormatAsLink(Name, Id)}.";
 
 		private const string AnimName = "fervine_kanim";
 		private const string AnimNameSeed = "seed_fervine_kanim";
@@ -24,8 +17,8 @@ namespace Fervine
 		{
 			var plantEntityTemplate = EntityTemplates.CreatePlacedEntity(
 				id: Id,
-				name: UI.FormatAsLink(Name, Id),
-				desc: Description,
+				name: STRINGS.PLANTS.FERVINE.NAME,
+				desc: STRINGS.PLANTS.FERVINE.DESC,
 				width: 1,
 				height: 1,
 				mass: 1f,
@@ -44,7 +37,7 @@ namespace Fervine
 				pressure_sensitive: false,
 				can_tinker: false,
 				baseTraitId: $"{Id}Original",
-				baseTraitName: Name);
+				baseTraitName: STRINGS.PLANTS.FERVINE.NAME);
 
 			var light2D = plantEntityTemplate.AddOrGet<Light2D>();
 			light2D.Color = new Color(1f, 1f, 0f);
@@ -56,15 +49,16 @@ namespace Fervine
 			var seed = EntityTemplates.CreateAndRegisterSeedForPlant(
 				plant: plantEntityTemplate,
 				id: SeedId,
-				name: UI.FormatAsLink(SeedName, Id),
-				desc: SeedDesc,
+				name: STRINGS.SEEDS.FERVINE.NAME,
+				desc: STRINGS.SEEDS.FERVINE.DESC,
 				productionType: SeedProducer.ProductionType.Hidden,
 				anim: Assets.GetAnim(AnimNameSeed),
 				numberOfSeeds: 0,
 				additionalTags: new List<Tag> { GameTags.DecorSeed },
 				sortOrder: 6,
 				width: 0.33f,
-				height: 0.33f);
+				height: 0.33f,
+				domesticatedDescription: STRINGS.PLANTS.FERVINE.DOMESTICATEDDESC);
 
 			EntityTemplates.CreateAndRegisterPreviewForPlant(
 				seed: seed,
