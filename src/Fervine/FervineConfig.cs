c33d2afa57a9703d1729893892b3e6dfe7c024e1
RealWiseSerpent <74019811+realwiseserpent@users.noreply.github.com>
2024-08-22 22:25:13 +0400

    Fervine - No seed drop fix

--- src/Fervine/FervineConfig.cs
+++ src/Fervine/FervineConfig.cs
@@ -5,91 +5,90 @@ using UnityEngine;
 
 namespace Fervine
 {
-	public class FervineConfig : IEntityConfig
-	{
-		public const string Id = "Fervine";
-		public const string Name = "Fervine";
-		public static string Description = $"A temperature reactive, subterranean {UI.FormatAsLink("Plant", "PLANTS")}.";
-		public static string DomesticatedDescription = $"Fervine uses tiny amounts of heat energy from the atmosphere to keep its light up. " +
-		                                               $"It won't use any if the temperature falls under {GameUtil.GetFormattedTemperature(293.15f)}.";
+    public class FervineConfig : IEntityConfig
+    {
+        public const string Id = "Fervine";
+        public const string Name = "Fervine";
+        public static string Description = $"A temperature reactive, subterranean {UI.FormatAsLink("Plant", "PLANTS")}.";
+        public static string DomesticatedDescription = $"Fervine uses tiny amounts of heat energy from the atmosphere to keep its light up. " +
+                                                       $"It won't use any if the temperature falls under {GameUtil.GetFormattedTemperature(293.15f)}.";
 
-		public const string SeedId = "FervineBulb";
-		public const string SeedName = "Fervine Bulb";
-		public static string SeedDesc = $"The {UI.FormatAsLink("Seed", "PLANTS")} of a {UI.FormatAsLink(Name, Id)}.";
+        public const string SeedId = "FervineBulb";
+        public const string SeedName = "Fervine Bulb";
+        public static string SeedDesc = $"The {UI.FormatAsLink("Seed", "PLANTS")} of a {UI.FormatAsLink(Name, Id)}.";
 
-		private const string AnimName = "fervine_kanim";
-		private const string AnimNameSeed = "seed_fervine_kanim";
+        private const string AnimName = "fervine_kanim";
+        private const string AnimNameSeed = "seed_fervine_kanim";
 
-		public GameObject CreatePrefab()
-		{
-			var plantEntityTemplate = EntityTemplates.CreatePlacedEntity(
-				id: Id,
-				name: UI.FormatAsLink(Name, Id),
-				desc: Description,
-				width: 1,
-				height: 1,
-				mass: 1f,
-				anim: Assets.GetAnim(AnimName),
-				initialAnim: "close",
-				sceneLayer: Grid.SceneLayer.BuildingFront,
-				decor: DECOR.BONUS.TIER3,
-				defaultTemperature: 350f);
+        public GameObject CreatePrefab()
+        {
+            var plantEntityTemplate = EntityTemplates.CreatePlacedEntity(
+                id: Id,
+                name: UI.FormatAsLink(Name, Id),
+                desc: Description,
+                width: 1,
+                height: 1,
+                mass: 1f,
+                anim: Assets.GetAnim(AnimName),
+                initialAnim: "close",
+                sceneLayer: Grid.SceneLayer.BuildingFront,
+                decor: DECOR.BONUS.TIER3,
+                defaultTemperature: 350f);
 
-			EntityTemplates.ExtendEntityToBasicPlant(
-				template: plantEntityTemplate,
-				temperature_lethal_low: 258.15f,
-				temperature_warning_low: 288.15f,
-				temperature_warning_high: 363.15f,
-				temperature_lethal_high: 373.15f,
-				pressure_sensitive: false,
-				can_tinker: false,
-				baseTraitId: $"{Id}Original",
-				baseTraitName: Name);
+            EntityTemplates.ExtendEntityToBasicPlant(
+                template: plantEntityTemplate,
+                temperature_lethal_low: 258.15f,
+                temperature_warning_low: 288.15f,
+                temperature_warning_high: 363.15f,
+                temperature_lethal_high: 373.15f,
+                pressure_sensitive: false,
+                can_tinker: false,
+                baseTraitId: $"{Id}Original",
+                baseTraitName: Name);
 
-			var light2D = plantEntityTemplate.AddOrGet<Light2D>();
-			light2D.Color = new Color(1f, 1f, 0f);
-			light2D.Lux = 1800;
-			light2D.Range = 5;
+            var light2D = plantEntityTemplate.AddOrGet<Light2D>();
+            light2D.Color = new Color(1f, 1f, 0f);
+            light2D.Lux = 1800;
+            light2D.Range = 5;
 
-			plantEntityTemplate.AddOrGet<Fervine>();
+            plantEntityTemplate.AddOrGet<Fervine>();
 
-			var seed = EntityTemplates.CreateAndRegisterSeedForPlant(
-				plant: plantEntityTemplate,
-				id: SeedId,
-				name: UI.FormatAsLink(SeedName, Id),
-				desc: SeedDesc,
-				productionType: SeedProducer.ProductionType.Hidden,
-				anim: Assets.GetAnim(AnimNameSeed),
-				numberOfSeeds: 0,
-				additionalTags: new List<Tag> { GameTags.DecorSeed },
-				sortOrder: 6,
-				width: 0.33f,
-				height: 0.33f);
+            var seed = EntityTemplates.CreateAndRegisterSeedForPlant(
+                plant: plantEntityTemplate,
+                id: SeedId,
+                name: UI.FormatAsLink(SeedName, Id),
+                desc: SeedDesc,
+                productionType: SeedProducer.ProductionType.Hidden,
+                anim: Assets.GetAnim(AnimNameSeed),
+                additionalTags: new List<Tag> { GameTags.DecorSeed },
+                sortOrder: 6,
+                width: 0.33f,
+                height: 0.33f);
 
-			EntityTemplates.CreateAndRegisterPreviewForPlant(
-				seed: seed,
-				id: "Heatbulb_preview",
-				anim: Assets.GetAnim(AnimName),
-				initialAnim: "place",
-				width: 1,
-				height: 1);
+            EntityTemplates.CreateAndRegisterPreviewForPlant(
+                seed: seed,
+                id: "Heatbulb_preview",
+                anim: Assets.GetAnim(AnimName),
+                initialAnim: "place",
+                width: 1,
+                height: 1);
 
-			SoundEventVolumeCache.instance.AddVolume("bristleblossom_kanim", "PrickleFlower_harvest", NOISE_POLLUTION.CREATURES.TIER3);
+            SoundEventVolumeCache.instance.AddVolume("bristleblossom_kanim", "PrickleFlower_harvest", NOISE_POLLUTION.CREATURES.TIER3);
 
-			return plantEntityTemplate;
-		}
+            return plantEntityTemplate;
+        }
 
-		public void OnPrefabInit(GameObject inst)
-		{
-		}
+        public void OnPrefabInit(GameObject inst)
+        {
+        }
 
-		public void OnSpawn(GameObject inst)
-		{
-		}
+        public void OnSpawn(GameObject inst)
+        {
+        }
 
-		public string[] GetDlcIds()
-		{
-			return DlcManager.AVAILABLE_ALL_VERSIONS;
-		}
-	}
+        public string[] GetDlcIds()
+        {
+            return DlcManager.AVAILABLE_ALL_VERSIONS;
+        }
+    }
 }
